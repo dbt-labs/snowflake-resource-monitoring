24592af70cae12e30e297468fc38fbcd233abb49
Matt Winkler <matt.winkler@fishtownanalytics.com>
2022-09-07 20:36:46 +0000

    update readme and correct set_query_tag macro

--- README.md
+++ README.md
@@ -1,15 +1,22 @@
-Welcome to your new dbt project!
+Welcom to the Snowflake Resource Monitoring Project!
 
-### Using the starter project
+NOTE: This is Snowflake-only at present (with a thought on how to extend to other warehouses :) ) 
 
-Try running the following commands:
-- dbt run
-- dbt test
+### Background
 
+Monitoring data warehouse utilization levels becomes a key consideration as analytics organizations mature and grow. In dbt, the [snowflake spend](https://hub.getdbt.com/gitlabhq/snowflake_spend/latest/) package has gained some traction as an approach for doing this. Snowflake spend is great for 
+analyzing workloads according to database, schema, user and warehouse. However, to really identify potential bottlenecks and areas for improvement, it's useful to go deeper by understanding the trends in model run times, configuration, user behavior in development vs. production environments, etc.
 
-### Resources:
-- Learn more about dbt [in the docs](https://docs.getdbt.com/docs/introduction)
-- Check out [Discourse](https://discourse.getdbt.com/) for commonly asked questions and answers
-- Join the [dbt community](http://community.getbdt.com/) to learn from other analytics engineers
-- Find [dbt events](https://events.getdbt.com) near you
-- Check out [the blog](https://blog.getdbt.com/) for the latest news on dbt's development and best practices
+### Setup
+
+Add the macro code from `config/set_query_tag.sql` to dbt Snowflake projects you want to monitor. This version of the package uses some dbt Cloud specific environment variables:
+
+- DBT_CLOUD_JOB_ID
+- DBT_CLOUD_RUN_ID
+- DBT_CLOUD_RUN_REASON
+
+### Potential Future Directions: 
+- Make this approach generic across Data Warehouses by logging the models and test runs in an `on-run-end` hook + macro.
+- Build additional reporting and pre-canned alerts
+- Implement anomaly detection on usage reporting time series (dbt Python models)
+- 
--- macros/config/set_query_tag.sql
+++ macros/config/set_query_tag.sql
@@ -1,23 +1,35 @@
 {% macro set_query_tag() -%}
   {# --TODO: extend this for tests once new test PR is merged #}
-  {% set dbt_environment_name = env_var('DBT_CLOUD_ENVIRONMENT_NAME', 'not set') %}
-  {% set dbt_project_name = project_name %}
+
+  {# -- These are built in dbt Cloud environment variables you can leverage to better understand your runs usage data #}
   {% set dbt_job_id = env_var('DBT_CLOUD_JOB_ID', 'not set') %}
   {% set dbt_run_id = env_var('DBT_CLOUD_RUN_ID', 'not set') %}
   {% set dbt_run_reason = env_var('DBT_CLOUD_RUN_REASON', 'development_and_testing') %}
+
+  {# -- These are built in to dbt Core #}
+  {% set dbt_project_name = project_name %}
   {% set dbt_user_name = target.user %}
-  {% set dbt_model_name = model.name %} 
-  
+  {% set dbt_model_name = model.name %}
+  {% set dbt_materialization_type = model.config.materialized %}
+  {% set dbt_incremental_full_refresh = 'false' %}
+  {% set dbt_environment_name = target.name %}
+
+  {% if dbt_materialization_type == 'incremental' and should_full_refresh() %}
+     {% set dbt_incremental_full_refresh = 'true' %}
+  {% endif %}
+
   {% if dbt_model_name %}
     
-    {% set new_query_tag = '{"dbt_project_name": "%s", "dbt_user_name": "%s", "dbt_model_name": "%s", "dbt_environment_name": "%s", "dbt_job_id": "%s", "dbt_run_id": "%s", "dbt_run_reason": "%s"}'
-      |format(dbt_project_name,
-              dbt_user_name,
-              dbt_model_name, 
-              dbt_environment_name,
+    {% set new_query_tag = '{"dbt_environment_name": "%s", "dbt_job_id": "%s", "dbt_run_id": "%s", "dbt_run_reason": "%s", "dbt_project_name": "%s", "dbt_user_name": "%s", "dbt_model_name": "%s", "dbt_materialization_type": "%s", "dbt_incremental_full_refresh": "%s"}'
+      |format(dbt_environment_name,
               dbt_job_id,
-              dbt_run_id,
-              dbt_run_reason) %}
+              dbt_run_id, 
+              dbt_run_reason,
+              dbt_project_name,
+              dbt_user_name,
+              dbt_model_name,
+              dbt_materialization_type,
+              dbt_incremental_full_refresh) %}
     {% set original_query_tag = get_current_query_tag() %}
     {{ log("Setting query_tag to '" ~ new_query_tag ~ "'. Will reset to '" ~ original_query_tag ~ "' after materialization.") }}
     {% do run_query("alter session set query_tag = '{}'".format(new_query_tag)) %}
